af90c8f680eebb8e0e375599265658607233df70
Benjamin Tejas <57376142+bentejas@users.noreply.github.com>
2024-12-18 18:58:47 +0000

    updated characters and voices, styling w border and bg img

--- src/pages/api/text-to-speech.ts
+++ src/pages/api/text-to-speech.ts
@@ -4,8 +4,6 @@ import type { APIRoute } from "astro";
 const userid = process.env.PLAYHT_USER_ID || "";
 const secretkey = process.env.PLAYHT_SECRET_KEY || "";
 
-// s3://voice-cloning-zero-shot/156a7b12-8e75-4f12-870d-d5436df5ec3a/original/manifest.json"
-
 PlayHT.init({
   userId: userid,
   apiKey: secretkey,
@@ -17,27 +15,28 @@ export const GET: APIRoute = async ({ request }): Promise<Response> => {
   try {
     const url = new URL(request.url);
     const text = url.searchParams.get("text") || "Hello world";
+    const voiceId =
+      url.searchParams.get("voiceId") ||
+      "s3://voice-cloning-zero-shot/9fc626dc-f6df-4f47-a112-39461e8066aa/oliviaadvertisingsaad/manifest.json";
 
-    // Stream the audio from PlayHT
-    const stream = await PlayHT.stream(text, { voiceEngine: "Play3.0-mini" });
+    const stream = await PlayHT.stream(text, {
+      voiceEngine: "Play3.0-mini",
+      voiceId: voiceId,
+    });
 
     const chunks: Uint8Array[] = [];
 
     for await (const chunk of stream) {
       if (typeof chunk === "string") {
-        // If chunk is a string, convert it to a Buffer using UTF-8 encoding
         const buffer = Buffer.from(chunk, "utf-8");
         chunks.push(new Uint8Array(buffer));
       } else if (Buffer.isBuffer(chunk)) {
-        // If chunk is a Buffer, convert it directly to Uint8Array
         chunks.push(new Uint8Array(chunk));
       } else {
-        // Handle unexpected chunk types
         console.warn("Received unknown chunk type:", typeof chunk);
       }
     }
 
-    // Combine all chunks into a single Blob
     const audioBlob = new Blob(chunks, { type: "audio/mpeg" });
 
     return new Response(audioBlob, {
