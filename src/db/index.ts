a8c0100d5ca14bb0183c6aebab5a800e6a9cfa9c
ben honda <bhonda89@gmail.com>
2024-12-19 15:49:10 +0000

    removing ws

--- src/db/index.ts
+++ src/db/index.ts
@@ -11,9 +11,9 @@
 
 // Websockets
 import { drizzle } from "drizzle-orm/neon-serverless";
-import ws from "ws";
+// import ws from "ws";
 
 export const db = drizzle({
   connection: process.env.DATABASE_URL!,
-  ws,
+  // ws,
 });
