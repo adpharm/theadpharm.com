6b04087af339532a2eacf1b90a3a79a238e4f0b8
ben honda <bhonda89@gmail.com>
2024-12-20 18:02:45 +0000

    images, sentry

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import { type Config } from "tailwindcss";
 import { zinc } from "tailwindcss/colors";
 import * as twAnimation from "tailwindcss-animate";
+import { plinkoSettings } from "@/lib/settings.plinko";
 
 export default {
   darkMode: "class", // or 'media' or 'class'
@@ -23,10 +24,11 @@ export default {
         gray: zinc,
       },
       backgroundImage: {
-        "snowfall-background": "url('/src/images/Snowfall_Background.png')",
-        BottomGraphics: "url('/src/images/BottomGraphics.png')",
-        goldenBall: "url('/src/images/temp-gold-ornament.png')",
-        giftBow: "url('/src/images/gift-bow.png')",
+        // "snowfall-background": "url('/src/images/Snowfall_Background.png')",
+        "snowfall-background": `url(${plinkoSettings.preloadImagesNonBlocking.snowfallBackground})`,
+        // BottomGraphics: "url('/src/images/BottomGraphics.png')",
+        // goldenBall: "url('/src/images/temp-gold-ornament.png')",
+        // giftBow: "url('/src/images/gift-bow.png')",
       },
       animation: {
         rev: "spin 4s cubic-bezier(.79,.14,.15,.86) infinite",
