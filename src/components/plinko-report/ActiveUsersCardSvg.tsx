22f2735ba63162317ba7837cf572f6399dec322c
albertzarifa <albert.zarifa@gmail.com>
2025-01-16 16:14:35 +0000

    Adding final changes to dashboard (pre-approval)

--- src/components/plinko-report/ActiveUsersCardSvg.tsx
+++ src/components/plinko-report/ActiveUsersCardSvg.tsx
@@ -1,11 +1,11 @@
-const StatsCardSvg = () => {
+const ActiveUsersCardSvg = () => {
   return (
     <div className="w-full flex justify-center">
-      <div className="relative w-full max-w-[416px] h-[119px]">
+      <div className="relative w-full pb-6 px-4 pt-0">
         <svg
           width="100%"
-          height="80%"
-          viewBox="0 0 416 119"
+          height="100%"
+          viewBox="0 0 416 126"
           preserveAspectRatio="xMidYMid meet"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -17,8 +17,11 @@ const StatsCardSvg = () => {
             </radialGradient>
           </defs>
 
+          {/* Here i'm using the active_users_sparkline4.svg */}
+          <path d="M1 115.641L36.7063 125L75.7902 115.641L83.993 106.283L116.804 115.641L156.371 125L173.259 106.283L206.07 97.8598L231.643 115.641L264 97.8598L289 24L305.5 78.4101L329.594 115.641L381.706 106.283L415 115.641" stroke="#22DB72" />
+
           {/* Path -- I'm using the file called active_users_sparkline3.svg */}
-          <path d="M32 87.8652L62.1002 95L95.0478 87.8652L101.963 80.7303L129.622 87.8652L162.977 95L177.213 80.7303L204.873 74.309L226.431 87.8652L253.708 74.309L274.783 18L288.692 59.4809L309.003 87.8652L352.934 80.7303L381 87.8652" stroke="#22DB72" />
+          {/* <path d="M32 87.8652L62.1002 95L95.0478 87.8652L101.963 80.7303L129.622 87.8652L162.977 95L177.213 80.7303L204.873 74.309L226.431 87.8652L253.708 74.309L274.783 18L288.692 59.4809L309.003 87.8652L352.934 80.7303L381 87.8652" stroke="#22DB72" /> */}
 
           {/* <path
             d="M1 93.151L36.7063 102L75.7902 93.151L83.993 84.3019L116.804 93.151L156.371 102L173.259 84.3019L206.07 76.3378L231.643 93.151L264 76.3378L289.063 2L305.5 57.9471L329.594 93.151L381.706 84.3019L415 93.151"
@@ -28,23 +31,23 @@ const StatsCardSvg = () => {
 
           {/* Static dot */}
           <circle
-            cx="274.783"
-            cy="18" //Move it down a little bit so its not hitting the top edge
+            cx="289"
+            cy="23" //Move it down a little bit so its not hitting the top edge
             r="4"
             fill="#22DB72"
           />
 
           {/* SVG ping animation */}
           <circle
-            cx="274.783"
-            cy="18" //Move it down a little bit so its not hitting the top edge
+            cx="289"
+            cy="23" //Move it down a little bit so its not hitting the top edge
             r="4"
             fill="url(#ping-gradient)"
           >
             <animate
               attributeName="r"
               from="4"
-              to="32"
+              to="25"
               dur="1s"
               repeatCount="indefinite"
             />
@@ -62,4 +65,4 @@ const StatsCardSvg = () => {
   );
 };
 
-export default StatsCardSvg;
+export default ActiveUsersCardSvg;
--- src/components/plinko-report/PlinkoReportDashboard.tsx
+++ src/components/plinko-report/PlinkoReportDashboard.tsx
@@ -8,14 +8,10 @@ import { convertUTCToDistanceToNow } from '@/lib/utils.plinko.date';
 import { ClientOnly } from '@/lib/utils.react-hydration';
 import { getPlinkoBallsDropped, getUserReplayCount } from '@/lib/server/plinkoReportLogic';
 import googleAnalyticsData from '@/data/googleAnalyticsOutput.json';
-import StatsCardSvg from './StatsCardSvg';
+import ActiveUsersCardSvg from './ActiveUsersCardSvg.tsx';
 import { FaInfoCircle } from "react-icons/fa";
 import { CustomTooltip } from '../Tooltip';
 import CountUp from 'react-countup';
-
-// Here is where i got shapes from – https://shapes.framer.website/
-// Edit shape color here – https://deeditor.com/
-// Adpharm red – #F2682B
 
 function PlinkoReportDashboard() {
   const leaderboardData = useStore($leaderboard);
@@ -41,58 +37,46 @@ function PlinkoReportDashboard() {
       {/* Top row */}
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 pt-6">
         {/* Games played */}
-        <Card className="md:order-1 col-span-1 bg-[#111111] text-white border-none h-full">
+        <Card className="md:order-1 bg-[#111111] text-white border-none flex flex-col justify-between">
           <CardHeader className="p-4">
-            <div className="flex items-center justify-between">
-              <CardTitle className="text-sm font-normal text-zinc-500">Games played</CardTitle>
-              <img src="/shape1.webp" className="pl-2 w-8" />
-            </div>
+            <CardTitle className="text-sm font-normal text-zinc-500">Games played</CardTitle>
           </CardHeader>
           <CardContent className="p-4 flex items-end justify-end h-fit">
-            <div className="text-3xl md:text-4xl font-bold">{totalGamesPlayed}</div>
+            <div className="text-4xl font-bold">{totalGamesPlayed}</div>
             {/* <CountUp className="text-3xl md:text-4xl font-bold" start={0} end={totalGamesPlayed} duration={5} decimals={0} prefix="" delay={0} separator=','/> */}
           </CardContent>
         </Card>
 
         {/* Plinko balls - single col */}
-        <Card className="md:order-2 col-span-1 lg:col-span-1 bg-[#111111] text-white border-none">
+        <Card className="md:order-2 lg:col-span-1 bg-[#111111] text-white border-none flex flex-col justify-between">
           <CardHeader className="p-4">
-            <div className="flex items-center justify-between">
-              <CardTitle className="text-sm font-normal text-zinc-500">Balls dropped</CardTitle>
-              <img src="/shape2.svg" className="pl-2 w-8" />
-            </div>
+            <CardTitle className="text-sm font-normal text-zinc-500">Balls dropped</CardTitle>
           </CardHeader>
           <CardContent className="p-4 flex items-end justify-end h-fit">
-            <div className="text-3xl md:text-4xl font-bold">{totalBallsDropped}</div>
+            <div className="text-4xl font-bold">{totalBallsDropped}</div>
             {/* <CountUp className="text-3xl md:text-4xl font-bold" start={0} end={totalBallsDropped} duration={5} decimals={0} prefix="" delay={0} separator=','/> */}
           </CardContent>
         </Card>
 
         {/* Total points - spans 2 cols */}
-        <Card className="md:hidden lg:grid lg:order-3 md:order-4 col-span-3 lg:col-span-2 bg-[#111111] text-white border-none">
+        <Card className="md:hidden lg:grid lg:order-3 md:order-4 col-span-2 lg:col-span-2 bg-[#111111] text-white border-none flex flex-col justify-between">
           <CardHeader className="p-4">
-            <div className="flex items-center justify-between">
-              <CardTitle className="text-sm font-normal text-zinc-500">Total points scored</CardTitle>
-              <img src="/shape3.svg" className="pl-2 w-8" />
-            </div>
+            <CardTitle className="text-sm font-normal text-zinc-500">Total points scored</CardTitle>
           </CardHeader>
-          <CardContent className="p-4 flex h-fit">
+          <CardContent className="p-4">
             {/* <div className="text-3xl md:text-4xl font-bold">{totalScores}</div>
             */}
-            <CountUp className="text-3xl md:text-4xl font-bold" start={0} end={totalScores} duration={10} decimals={0} prefix="" delay={0} separator=',' />
+            <CountUp className="text-3xl md:text-4xl font-bold" start={0} end={totalScores} duration={3} decimals={0} prefix="" delay={0} separator=',' />
           </CardContent>
         </Card>
 
         {/* Rounds Played */}
         <Card className="lg:order-4 md:order-3 cols-span-3 bg-[#111111] text-white border-none h-full">
           <CardHeader className="p-4">
-            <div className="flex items-center justify-between">
-              <CardTitle className="text-sm font-normal text-zinc-500">Rounds played</CardTitle>
-              <img src="/shape4.svg" className="pl-2 w-8" />
-            </div>
+            <CardTitle className="text-sm font-normal text-zinc-500">Rounds played</CardTitle>
           </CardHeader>
           <CardContent className="p-4 flex items-end justify-end">
-            <div className="text-3xl md:text-4xl font-bold">{totalRoundsPlayed}</div>
+            <div className="text-4xl font-bold">{totalRoundsPlayed}</div>
           </CardContent>
         </Card>
 
@@ -112,56 +96,43 @@ function PlinkoReportDashboard() {
         </div>
 
         {/* Total points - spans 2 cols */}
-        <Card className="hidden md:grid lg:hidden lg:order-3 md:order-4 col-span-1 md:col-span-2 bg-[#111111] text-white border-none">
+        <Card className="hidden md:grid lg:hidden lg:order-3 md:order-4 col-span-2 md:col-span-2 bg-[#111111] text-white border-none flex flex-col justify-between">
           <CardHeader className="p-4">
-            <div className="flex items-center justify-between">
-              <CardTitle className="text-sm font-normal text-zinc-500">Total points scored</CardTitle>
-              <img src="/shape3.svg" className="pl-2 w-8" alt="Logo" />
-            </div>
+            <CardTitle className="text-sm font-normal text-zinc-500">Total points scored</CardTitle>
           </CardHeader>
-          <CardContent className="p-4 flex lg:items-end lg:justify-end h-fit">
-            {/* <div className="text-3xl md:text-4xl font-bold">{totalScores}</div> */}
+          <CardContent className="p-4 flex md:justify-end lg:items-end lg:justify-end">
             <CountUp className="text-3xl md:text-4xl font-bold" start={0} end={totalScores} duration={10} decimals={0} prefix="" delay={0} separator=',' />
           </CardContent>
         </Card>
 
-        <Card className="col-span-2 md:col-span-2 lg:col-span-3 bg-[#111111] text-white border-none">
+        <Card className="col-span-2 md:col-span-2 lg:col-span-3 bg-[#111111] text-white border-none flex flex-col justify-between">
           <CardHeader className="p-4">
-            <div className="flex items-center justify-between">
-              <CardTitle className="text-sm font-normal text-zinc-500">Time spent playing</CardTitle>
-              <img src="/shape5.svg" className="pl-2 w-8" alt="Logo" />
-            </div>
+            <CardTitle className="text-sm font-normal text-zinc-500">Time spent playing</CardTitle>
           </CardHeader>
           <CardContent className="p-4 flex md:items-end md:justify-end lg:items-start lg:justify-start h-fit">
-            <div className="text-3xl md:text-4xl font-bold">{timeSpentPlayingPlinko}</div>
+            <div className="text-2xl md:text-3xl font-bold">{timeSpentPlayingPlinko}</div>
           </CardContent>
         </Card>
 
         {/* Play button */}
-        <Card className="col-span-1 bg-[#111111] text-white border-none">
+        <Card className="col-span-1 bg-[#111111] text-white border-none flex flex-col justify-between">
           <CardHeader className='p-4 pb-2'>
-            <div className="flex items-center justify-between">
-              <CardTitle className="text-sm font-normal text-zinc-500">Let's play!</CardTitle>
-              <img src="/shape6.svg" className="pl-2 w-8" />
-            </div>
+            <CardTitle className="text-sm font-normal text-zinc-500">Let's play!</CardTitle>
           </CardHeader>
-          <CardContent className="p-4 pb-1 flex justify-center pt-0">
+          <CardContent className="p-4 flex justify-center pt-0">
             <a href='/digital/plinko'>
-              <img src="/play-button.png" className="w-20 md:w-16" alt="Play" />
+              <img src="/play-button.png" className="w-20" alt="Play" />
             </a>
           </CardContent>
         </Card>
 
         {/* Players count */}
-        <Card className="col-span-1 bg-[#111111] text-white border-none">
+        <Card className="col-span-1 bg-[#111111] text-white border-none flex flex-col justify-between">
           <CardHeader className="p-4">
-            <div className="flex items-center justify-between">
-              <CardTitle className="text-sm font-normal text-zinc-500">Players</CardTitle>
-              <img src="/shape7.svg" className="pl-2 w-8" />
-            </div>
+            <CardTitle className="text-sm font-normal text-zinc-500">Players</CardTitle>
           </CardHeader>
           <CardContent className="p-4 flex md:items-end md:justify-end h-fit">
-            <div className="text-3xl md:text-4xl font-bold">{totalUniqueUsers}</div>
+            <div className="text-6xl font-medium">{totalUniqueUsers}</div>
           </CardContent>
         </Card>
       </div>
@@ -169,32 +140,28 @@ function PlinkoReportDashboard() {
       {/* Last section */}
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 items-start">
         <div className="col-span-2 md:col-span-4 lg:col-span-3 grid grid-cols-3 gap-4">
-
           {/* Active users box */}
-          <Card className="col-span-3 md:col-span-2 bg-[#111111] text-white border-none h-full">
-            <CardHeader className="p-4">
+          <Card className="col-span-3 md:col-span-2 bg-[#111111] text-white border-none flex flex-col justify-between">
+            <CardHeader className="p-4 pb-0">
               {/* <CardTitle>Active users</CardTitle> */}
-              <div className="flex items-center justify-between">
-                <div>
-                  <div className='flex items-center gap-2 pt-0 text-sm md:text-md m-0'>
-                    <span className="text-green-500 font-bold">+1,448%</span>
-                    <span>active users</span>
-                  </div>
+              <div>
+                <div className='flex items-center gap-2 pt-0 xs:text-lg m-0 font-bold'>
+                  <span className="text-green-500">+1,448%</span>
+                  <span>active users</span>
                 </div>
-                <img src="/shape8.svg" className="pl-2 w-8 pb-0" />
               </div>
               <CardTitle className="text-sm font-normal text-zinc-500 !mt-0">on theadpharm.com
               </CardTitle>
 
             </CardHeader>
-            <CardContent className="p-4 pt-0 md:pb-0">
+            <CardContent className="!p-0">
 
               {/* <span className="animate-ping absolute inline-flex h-2 w-2 rounded-full bg-green-400 opacity-75 left-[42.7%]"></span> */}
               {/* <img className='relative' src="/active_users_sparkline.svg" alt="Active users trend" /> 
                 */}
-              <div className="flex justify-center md:pb-2">
-                <div className="relative w-full max-w-[416px] h-[103px]">
-                  <StatsCardSvg />
+              <div className="flex justify-center w-full">
+                <div className="relative w-full">
+                  <ActiveUsersCardSvg />
                 </div>
               </div>
             </CardContent>
@@ -206,26 +173,24 @@ function PlinkoReportDashboard() {
           </div>
 
           {/* Replays count */}
-          <Card className="col-span-2 md:col-span-1 bg-[#111111] text-white border-none h-full">
+          <Card className="col-span-2 md:col-span-1 bg-[#111111] text-white border-none h-full flex flex-col justify-between">
             <CardHeader className="p-4">
-              <div className="flex items-center justify-between">
-                <CardTitle className="text-sm font-normal text-zinc-500">Replays</CardTitle>
-                {/* <FaInfoCircle /> */}
+              <CardTitle className="text-sm font-normal text-zinc-500 flex items-center">
+                <span>Replays</span>
                 <CustomTooltip
                   preview={
-                    <div className='cursor-pointer'>
-                      <FaInfoCircle size={24} />
-                    </div>
+                    <span className='cursor-pointer ml-1'>
+                      <FaInfoCircle size={10} />
+                    </span>
                   }
                   hoverText="The number of games that are played by a user after their first game."
                   hoverTextCustomClass="text-xs w-40 opacity-100"
                   doYouWantDelay={true}
                 />
-              </div>
-
+              </CardTitle>
             </CardHeader>
-            <CardContent className="p-4 md:pt-12 flex md:items-end md:justify-end h-fit">
-              <div className="text-3xl md:text-4xl font-bold">{totalReplays}</div>
+            <CardContent className="p-4 md:pt-12 flex items-end justify-end h-fit">
+              <div className="text-6xl font-medium">{totalReplays}</div>
             </CardContent>
           </Card>
         </div>
@@ -233,13 +198,10 @@ function PlinkoReportDashboard() {
 
         {/* Leaderboard */}
         <Card className="col-span-2 md:col-span-4 lg:col-span-3 bg-[#111111] text-white border-none">
-          <CardHeader className="p-4">
-            <div className="flex items-center justify-between">
-              <CardTitle>Leaderboard</CardTitle>
-              <img src="/shape9.svg" className="pl-2 w-8" />
-            </div>
+          <CardHeader className="p-6">
+            <CardTitle>Leaderboard</CardTitle>
           </CardHeader>
-          <CardContent className="p-4 max-h-72 overflow-auto no-scrollbar">
+          <CardContent className="p-6 max-h-72 overflow-auto no-scrollbar">
             <Table>
               {leaderboardData.map((rowData) => (
                 <TableRow key={`game-${rowData.game.id}`}>
